a99dede9f421c30c086404e26e5b3e6772a40508
lehonganhphap1009-ux <lehonganhphap1009@gmail.com>
2026-07-07 18:31:14 +0000

    Lưu code pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,12 @@
+import sys
+
+import pandas as pd
+
+print("arguments:", sys.argv)
+
+month = int(sys.argv[1])    
+
+df = pd.DataFrame({"day": [1, 2, 3],"num_passengers": [4, 5, 6] })
+df["month"] = month
+print(f"hello world, month = {month}")
+print(df)
