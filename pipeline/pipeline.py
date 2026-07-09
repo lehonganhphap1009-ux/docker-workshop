8e5897bc972a8a983377ea201f613ad5c8025740
lehonganhphap1009-ux <lehonganhphap1009@gmail.com>
2026-07-09 06:56:50 +0000

    pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,12 +1,23 @@
+import os
 import sys
 
 import pandas as pd
 
 print("arguments:", sys.argv)
 
-month = int(sys.argv[1])    
+if len(sys.argv) < 2:
+    raise ValueError("Usage: python pipeline.py <month>")
 
-df = pd.DataFrame({"day": [1, 2, 3],"num_passengers": [4, 5, 6] })
+month = int(sys.argv[1])
+
+os.makedirs("data", exist_ok=True)
+
+df = pd.DataFrame({"day": [1, 2, 3], "num_passengers": [4, 5, 6]})
 df["month"] = month
+
+df.to_parquet(f"data/output_{month}.parquet", engine="pyarrow", index=False)
+
 print(f"hello world, month = {month}")
 print(df)
+
+
